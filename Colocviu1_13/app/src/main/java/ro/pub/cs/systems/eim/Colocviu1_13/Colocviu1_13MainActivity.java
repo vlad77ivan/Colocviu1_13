000ecb32595074dbedddbfffcbe94a691c9ca247
Vlad Ivan <->
2020-04-13 10:22:34 +0300

    A

---
+++ Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colocviu1_13MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu1_13_main);
+    }
+}
