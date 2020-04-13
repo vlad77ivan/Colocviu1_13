0c2ff9917a5ae077668fde2a8c58443ebc423b7e
Vlad Ivan <->
2020-04-13 10:27:02 +0300

    A fixed

--- Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
+++ Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
@@ -3,12 +3,20 @@ package ro.pub.cs.systems.eim.Colocviu1_13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Colocviu1_13MainActivity extends AppCompatActivity {
+
+    private Button northButton, southButton, eastButton, westButton;
+    private TextView pressedView;
+    private Button navigateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colocviu1_13_main);
+
+
     }
 }
